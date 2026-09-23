e9eaf599a31824ec551e7ddf6e42fbd58638dfd9
Armin Smajlagic <45321513+ArminSmajlagic@users.noreply.github.com>
2026-09-23 11:31:55 +0200

    fix routing

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home/Naslovna.tsx";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <App />,
+            children: [
+                {
+                    index: true,
+                    element: <Home />,
+                },
+            ],
+        },
+    ],
     {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-            },
-        ],
+        basename: "/beauty-salon-app",
     },
-]);
+);
